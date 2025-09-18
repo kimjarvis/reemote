ee6c28c8c50c3a4121faec9c5d26f80269d8ab59
kimjarvis <kim.jarvis@tpfsystems.com>
2025-09-18 15:25:41 +0100

    Package Controller documentation

--- localhost.py
+++ localhost.py
@@ -2,6 +2,6 @@ from typing import List, Tuple, Dict, Any
 
 def inventory() -> List[Tuple[Dict[str, Any], Dict[str, str]]]:
     return [({'host': 'localhost',  # Host
-                'username': 'kim',  # User name
-                'password': 'xnjs'  # Password
+                'username': 'username',  # User name
+                'password': 'password'  # Password
     },{})]
