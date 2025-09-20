78d4cc27c9fafec3b5687167a0d7097ec9e93809
kimjarvis <kim.jarvis@tpfsystems.com>
2025-09-20 10:02:00 +0100

    Separate the GUI from the CLI

--- reemote/deploy_manager/main1.py
+++ reemote/deploy_manager/main1.py
@@ -14,15 +14,25 @@ from nicegui import app, events, ui
 from reemote.validate_inventory_structure import validate_inventory_structure
 from reemote.verify_inventory_connect import verify_inventory_connect
 
-class Gui:
+class Execution_report:
     def __init__(self):
         self.columns = [{'headerName': 'Command', 'field': 'command'}]
         self.rows = []
-        self.inventory = None
 
     def set(self, columns, rows):
         self.columns = columns
         self.rows = rows
+
+    @ui.refreshable
+    def execution_report(self):
+        return ui.aggrid({
+            'columnDefs': self.columns,
+            'rowData': self.rows,
+        }).classes('max-h-40  overflow-y-auto')
+
+class Inventory_upload:
+    def __init__(self):
+        self.inventory = None
 
     async def handle_upload(self, e: events.UploadEventArguments):
         text = e.content.read().decode('utf-8')
@@ -36,28 +46,10 @@ class Gui:
         ui.notify("Inventory structure and all hosts connect")
         self.inventory = inventory()
 
-        # Start with the fixed column definition for "Command"
-        columnDefs = [{'headerName': 'Command', 'field': 'command'}]
-
-        # Dynamically generate column definitions for each host
-        for index, (host_info, _) in enumerate(inventory()):
-            host_ip = host_info['host']
-            columnDefs.append({'headerName': f'{host_ip} Executed', 'field': f'{host_ip.replace(".","_")}_executed'})
-            columnDefs.append({'headerName': f'{host_ip} Changed', 'field': f'{host_ip.replace(".","_")}_changed'})
-        self.columns = columnDefs
-        self.execution_report.refresh()
-
-    @ui.refreshable
-    def execution_report(self):
-        return ui.aggrid({
-            'columnDefs': self.columns,
-            'rowData': self.rows,
-        }).classes('max-h-40  overflow-y-auto')
-
-    def upload_inventory(self):
-        return ui.upload(label="UPLOAD INVENTORY",
-             on_upload=self.handle_upload,  # Handle the file upload
-        ).props('accept=.py').classes('max-w-full')
+    # def upload_inventory(self):
+    #     return ui.upload(label="UPLOAD INVENTORY",
+    #          on_upload=self.handle_upload,  # Handle the file upload
+    #     ).props('accept=.py').classes('max-w-full')
 
 
 class Stdout_report:
@@ -76,7 +68,7 @@ class Stdout_report:
             'rowData': self.rows,
         }).classes('max-h-40  overflow-y-auto')
 
-class Sources:
+class Sources_upload:
     def __init__(self):
         self.source= "/"
         self._classes = []
@@ -104,8 +96,24 @@ class Wrapper:
         # The result is available in stdout
         # print(r.cp.stdout)
 
+async def inv_upload(inv, er, stdout, sources):
+    # Start with the fixed column definition for "Command"
+    columns = [{'headerName': 'Command', 'field': 'command'}]
+    rows = []
 
-async def run_the_deploy(gui, stdout, sources):
+    # Dynamically generate column definitions for each host
+    for index, (host_info, _) in enumerate(inv.inventory):
+        host_ip = host_info['host']
+        columns.append({'headerName': f'{host_ip} Executed', 'field': f'{host_ip.replace(".","_")}_executed'})
+        columns.append({'headerName': f'{host_ip} Changed', 'field': f'{host_ip.replace(".","_")}_changed'})
+    print(columns)
+    er.set(columns, rows)
+    er.execution_report.refresh()
+    stdout.set(columns, rows)
+    stdout.execution_report.refresh()
+
+
+async def run_the_deploy(inv, er, stdout, sources):
     if sources.source != "/":
         if sources.source and sources.deployment:
             if not verify_source_file_contains_valid_class(sources.source, sources.deployment):
@@ -119,26 +127,42 @@ async def run_the_deploy(gui, stdout, sources):
             print("root class not found")
             sys.exit(1)
 
-        responses = await execute(gui.inventory, Wrapper(root_class))
+        responses = await execute(inv.inventory, Wrapper(root_class))
         c, r =produce_grid(produce_json(responses))
-        gui.set(c, r)
-        gui.execution_report.refresh()
+        er.set(c, r)
+        # print("trace er")
+        # print(c)
+        # print(r)
+        er.execution_report.refresh()
         c, r =produce_output_grid(produce_json(responses))
         stdout.set(c, r)
+        # print("trace stdout")
+        # print(c)
+        # print(r)
         stdout.execution_report.refresh()
 
 
 @ui.page('/')
 def page():
-    gui = Gui()
-    stdout = Stdout_report()
-    sources = Sources()
-    gui.upload_inventory()
+    sr = Stdout_report()
+    er = Execution_report()
+    sources = Sources_upload()
+    inv = Inventory_upload()
+
+    async def combined_upload_handler(e):
+        await inv.handle_upload(e)  # Handle the upload first
+        await inv_upload(inv, er, sr, sources)  # Then run your setup logic
+
+    ui.upload(
+        label="UPLOAD INVENTORY",
+        on_upload=combined_upload_handler
+    ).props('accept=.py').classes('max-w-full')
+
     ui.button('Upload Source', on_click=lambda: sources.pick_file(), icon='folder')
     sources.classes()
-    ui.button('Deploy', on_click=lambda: run_the_deploy(gui, stdout, sources))
-    stdout.execution_report()
-    gui.execution_report()
+    ui.button('Deploy', on_click=lambda: run_the_deploy(inv, er, sr, sources))
+    sr.execution_report()
+    er.execution_report()
 
 ui.run(title="Deployment Manager", reload=False, port=native.find_open_port(),
        storage_secret='private key to secure the browser session cookie')
