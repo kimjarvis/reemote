77d4e3bd95dc2c5dc8993bc572fd171d52bda163
kimjarvis <kim.jarvis@tpfsystems.com>
2025-09-27 12:10:14 +0100

    Fix read file and pandas

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))  # Path to ~/reemo
 project = 'Reemote'
 copyright = '2025, Kim Jarvis'
 author = 'Kim Jarvis'
-release = '0.0.13'
+release = '0.0.14'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
