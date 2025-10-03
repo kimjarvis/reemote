64b3c02fe026187e5ede233d59eb1af6b65b392d
kimjarvis <kim.jarvis@tpfsystems.com>
2025-10-03 13:26:33 +0100

    Release 0.0.16

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="reemote",  # Name of your package
-    version="0.0.15",   # Version number
+    version="0.0.16",   # Version number
     description="A Python package for reemote functionality",  # Short description
     long_description="""
     Reemote is a Python API for task automation, configuration management and application deployment.
@@ -26,6 +26,7 @@ setup(
         "tabulate",
         "fastapi",
         "nicegui",
+        "pandas",
     ],  # List of dependencies with proper commas
     extras_require={
         "dev": [
