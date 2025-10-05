55406bef2c2eb1b976d9adfbeb9b60f0e52c9817
kimjarvis <kim.jarvis@tpfsystems.com>
2025-10-05 16:19:15 +0100

    pip

--- reemote/operations/pip/packages.py
+++ reemote/operations/pip/packages.py
@@ -1,5 +1,8 @@
 from typing import List
 from reemote.operation import Operation
+from reemote.facts.pip.get_packages import Get_packages
+from reemote.commands.pip.install import Install
+from reemote.commands.pip.remove import Remove
 
 class Packages:
     """
@@ -62,15 +65,19 @@ class Packages:
         r0.executed = self.guard
 
         # Retrieve the current list of installed packages
-        r1 = yield Operation(f"pip list --format=json",guard=self.guard, sudo=self.sudo, su=self.su)
+        # r1 = yield Operation(f"pip list --format=json",guard=self.guard, sudo=self.sudo, su=self.su)
+        r1 = yield Get_packages()
 
         # Add or remove packages based on the `present` flag
-        r2 = yield Operation(f"pip install -y {self.op}",guard=self.guard and self.present, sudo=self.sudo, su=self.su)
+        # r2 = yield Operation(f"pip install -y {self.op}",guard=self.guard and self.present, sudo=self.sudo, su=self.su)
+        r2 = yield Install(self.packages,self.guard and self.present, self.sudo, self.su)
 
-        r3 = yield Operation(f"pip uninstall -y {self.op}",guard=self.guard and not self.present, sudo=self.sudo, su=self.su)
+        # r3 = yield Operation(f"pip uninstall -y {self.op}",guard=self.guard and not self.present, sudo=self.sudo, su=self.su)
+        r3 = yield Remove(self.packages,self.guard and not self.present, self.sudo, self.su)
 
         # Retrieve the updated list of installed packages
-        r4 = yield Operation(f"pip list --format=json",guard=self.guard, sudo=self.sudo, su=self.su)
+        # r4 = yield Operation(f"pip list --format=json",guard=self.guard, sudo=self.sudo, su=self.su)
+        r4 = yield Get_packages()
 
         # Set the `changed` flag iff the package state has changed
         if self.guard and (r1.cp.stdout != r4.cp.stdout):
