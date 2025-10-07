30f49c92f2a7cdaad1eb77a343b7e9f39d732ff4
kimjarvis <kim.jarvis@tpfsystems.com>
2025-10-07 21:48:58 +0100

    update

---
+++ inventory-proxmox-alpine.py
@@ -0,0 +1,16 @@
+from typing import List, Tuple, Dict, Any
+
+def inventory() -> List[Tuple[Dict[str, Any], Dict[str, str]]]:
+    return [
+        (
+            {
+                'host': '192.168.1.56',  # images:debian/13 debian-0
+                'username': 'kim',  # Kim Jarvis
+                'password': 'kim',  # Password
+            },
+            {
+                'sudo_user': 'kim',  # Sudo user
+                'sudo_password': 'kim',  # Password
+            }
+        )
+    ]
